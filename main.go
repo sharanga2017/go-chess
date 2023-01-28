ca7a1c982b5fcb504cfdfc5d0eacf5344c772490
sharanga2017 <serguei.khan@gmail.com~>
2023-01-28 22:45:00 +0100

    game start

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ package main
 
 import (
 	"image/color"
+	"math/rand"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -17,6 +19,16 @@ func main() {
 	w := a.NewWindow("chess")
 	game := chess.NewGame()
 	grid := createGrid(game.Position().Board())
+
+	go func() {
+		rand.Seed(time.Now().Unix())
+		for game.Outcome() == chess.NoOutcome {
+			time.Sleep(time.Microsecond * 50000)
+			valid := game.ValidMoves()
+			m := valid[rand.Intn(len(valid))]
+			move(m, game, grid)
+		}
+	}()
 
 	w.SetContent(grid)
 	w.Resize(fyne.NewSize(480, 480))
@@ -42,4 +54,25 @@ func createGrid(b *chess.Board) *fyne.Container {
 
 	return grid
 
+}
+
+func move(m *chess.Move, game *chess.Game, grid *fyne.Container) {
+	game.Move(m)
+	refreshGrid(grid, game.Position().Board())
+}
+
+func refreshGrid(grid *fyne.Container, b *chess.Board) {
+	y, x := 7, 0
+	for _, cell := range grid.Objects {
+		p := b.Piece(chess.Square(x + y*8))
+		img := cell.(*fyne.Container).Objects[1].(*canvas.Image)
+		img.Resource = ressourceForPiece(p)
+		img.Refresh()
+
+		x++
+		if x == 8 {
+			x = 0
+			y--
+		}
+	}
 }
